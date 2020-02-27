7f3fe62ebbaf933b7c3b6db641cb597d595e0c9a
sickdaflip <philippbreitsprecher@gmail.com>
2020-02-27 17:29:45 +0100

    add animation to product&category grids

--- skin/frontend/foundation/default/src/assets/js/app.js
+++ skin/frontend/foundation/default/src/assets/js/app.js
@@ -94,4 +94,41 @@ $(document).ready(function () {
 
     Foundation.reInit($('.categories-grid'));
     Foundation.reInit($('.categories-home-grid'));
+
+    $.fn.visible = function(partial) {
+
+            var $t            = $(this),
+                $w            = $(window),
+                viewTop       = $w.scrollTop(),
+                viewBottom    = viewTop + $w.height(),
+                _top          = $t.offset().top,
+                _bottom       = _top + $t.height(),
+                compareTop    = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
+
+            return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+    var win = $(window);
+    var allMods = $(".item");
+
+    allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("already-visible");
+        }
+    });
+
+    win.scroll(function(event) {
+
+        allMods.each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("come-in");
+            }
+        });
+
+    });
+
 });
