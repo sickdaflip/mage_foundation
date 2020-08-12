5d9bf1bfb83c6b396e99e82b5a9db272ff4260d6
sickdaflip <philippbreitsprecher@gmail.com>
2020-08-12 14:57:43 +0200

    .gitignore fix

---
+++ skin/frontend/foundation/default/src/assets/js/app.js
@@ -0,0 +1,132 @@
+import $ from 'jquery';
+import {whatInput} from 'what-input';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+$(document).ready(function () {
+
+    $(document).foundation();
+
+    // Hamburgers is-active on OffCanvas
+    $('#offCanvas').on('opened.zf.offCanvas', function() {
+        $('.hamburger').addClass('is-active');
+    });
+    $('#offCanvas').on('closed.zf.offCanvas', function() {
+        $('.hamburger').removeClass('is-active');
+    });
+
+    // not hide do remove!
+    if (Foundation.MediaQuery.is('small only')) {
+        $('.hide-for-small-only').remove();
+    }
+    // Alert-Box auto close
+    $('.callout.messages').slideDown({
+        duration: 1500,
+        complete: function () {
+            $('.callout.messages').delay(7000).slideUp(1500);
+        }
+    });
+
+    //Grid / List view
+    $('.view-mode strong.grid').after('<i class="fa fa-th"></i>');
+    $('.view-mode strong.list').after('<i class="fa fa-align-justify"></i>');
+
+    $('.view-mode a.list').each(function () {
+        if ($(this).text() == 'List')
+            $(this).text('');
+        $(this).append('<i class="fa fa-align-justify"></i>');
+    });
+
+    $('.view-mode a.grid').each(function () {
+        if ($(this).text() == 'Grid')
+            $(this).text('');
+        $(this).append('<i class="fa fa-th"></i>');
+    });
+
+    //Product Option show more and less
+    $('ul.options-list').not('ul.product-bundle-radio').each(function(){
+
+        var LiN = $(this).find('li').length;
+
+        if( LiN > 3){
+            $('li', this).eq(2).nextAll().hide().addClass('toggleable');
+            $(this).append('<li class="more">weitere Optionen anzeigen</li>');
+        }
+
+    });
+
+    $('ul.options-list').on('click','.more', function(){
+        if( $(this).hasClass('less') ){
+            $(this).text('weitere Optionen anzeigen').removeClass('less');
+        }else{
+            $(this).text('weitere Optionen ausblenden').addClass('less');
+        }
+        $(this).siblings('li.toggleable').slideToggle();
+    });
+
+    //only for Jura
+    if (Foundation.MediaQuery.atLeast('medium')) {
+      if ($("[class*='categorypath-hersteller-jura'] .mb-mana-catalog-leftnav").length) {
+          var $header = $("[class*='categorypath-hersteller-jura'] .mb-mana-catalog-leftnav").first();
+
+          // This is the odd behaviour I was speaking about. It seems as though the sizes
+          // aren't being calculated when initialized programmatically. We're cathing the
+          // "init" event triggered by the plug-in when it's successfully initialized on the
+          // element. Then we're forcing it to run the "_calc" function manually.
+          $header.one('init.zf.sticky', function () {
+              $(this).foundation('_calc', true);
+          });
+
+          // We're ready to initialize the plug-in, just like described in the documentation.
+          $header.$sticky = new Foundation.Sticky($header, {
+              anchor: 'main'
+          });
+      }
+        $("[class*='categorypath-hersteller-jura'] .block-layered-nav .block-title").before('<img src="https://www.gastrodax.de/media/wysiwyg/manufacturer/jura-mp.png" alt="Jura Logo" width="150" class="show-for-large">');
+    }
+
+    Foundation.reInit($('.categories-grid'));
+    Foundation.reInit($('.categories-home-grid'));
+
+    $.fn.visible = function(partial) {
+
+            var $t            = $(this),
+                $w            = $(window),
+                viewTop       = $w.scrollTop(),
+                viewBottom    = viewTop + $w.height(),
+                _top          = $t.offset().top,
+                _bottom       = _top + $t.height(),
+                compareTop    = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
+
+            return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+    var win = $(window);
+    var allMods = $(".item");
+
+    allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("already-visible");
+        }
+    });
+
+    win.scroll(function(event) {
+
+        allMods.each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("come-in");
+            }
+        });
+
+    });
+
+});
