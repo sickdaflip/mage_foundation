968df2704e49e965f42ecc0215caab78a04e460d
sickdaflip <philippbreitsprecher@gmail.com>
2021-03-03 10:07:51 +0100

    remove sitemap generation

--- app/code/local/Foundation/Sitemap/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Foundation_Sitemap_Helper_Data extends Mage_Core_Helper_Data
-{
-
-}
--- app/code/local/Foundation/Sitemap/Model/Sitemap/Amasty/Pure.php
+++
@@ -1,3 +0,0 @@
-<?php /* added automatically by conflict fixing tool */ if (Mage::getConfig()->getNode('modules/Creare_CreareSeoCore/active')) {
-            class Foundation_Sitemap_Model_Sitemap_Amasty_Pure extends Creare_CreareSeoCore_Model_Sitemap_Sitemap {}
-        } else { class Foundation_Sitemap_Model_Sitemap_Amasty_Pure extends Mage_Sitemap_Model_Sitemap {} } ?>
