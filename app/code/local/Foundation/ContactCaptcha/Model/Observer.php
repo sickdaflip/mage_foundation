b77576ca2486d3195b2faaf5ba85a49aad284b69
sickdaflip <philippbreitsprecher@gmail.com>
2024-11-13 16:08:22 +0100

    add captcha to contactForm

--- app/code/local/Foundation/ContactCaptcha/Model/Observer.php
+++ app/code/local/Foundation/ContactCaptcha/Model/Observer.php
@@ -1,18 +1,31 @@
 <?php
 class Foundation_ContactCaptcha_Model_Observer extends Mage_Captcha_Model_Observer
 {
-    public function checkContacts($observer)
-    {
-        $formId = 'contactForm';
+    public function checkContacts($observer){
+        $formId = 'contacts';
         $captchaModel = Mage::helper('captcha')->getCaptcha($formId);
-        if ($captchaModel->isRequired()) {
+        if ($captchaModel->isRequired()){
             $controller = $observer->getControllerAction();
-            if (!$captchaModel->isCorrect($this->_getCaptchaString($controller->getRequest(), $formId))) {
+            $word = $this->_getCaptchaString($controller->getRequest(), $formId);
+            if (!$captchaModel->isCorrect($word)) {
                 Mage::getSingleton('customer/session')->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
                 $controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
-                $controller->getResponse()->setRedirect(Mage::getUrl('*/*/'));
+                $url =  Mage::getUrl('contacts');
+                $controller->getResponse()->setRedirect($url);
             }
         }
         return $this;
+    }
+    /**
+     * Get Captcha String
+     *
+     * @param Varien_Object $request
+     * @param string $formId
+     * @return string
+     */
+    protected function _getCaptchaString($request, $formId)
+    {
+        $captchaParams = $request->getPost(Mage_Captcha_Helper_Data::INPUT_NAME_FIELD_VALUE);
+        return $captchaParams[$formId];
     }
 }
