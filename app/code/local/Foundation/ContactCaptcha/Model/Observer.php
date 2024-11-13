3e30c7d5462722dbc45e076034a98c8ca3b79962
sickdaflip <philippbreitsprecher@gmail.com>
2024-11-13 14:12:58 +0100

    add captcha to contactForm

---
+++ app/code/local/Foundation/ContactCaptcha/Model/Observer.php
@@ -0,0 +1,18 @@
+<?php
+class Foundation_ContactCaptcha_Model_Observer extends Mage_Captcha_Model_Observer
+{
+    public function checkContacts($observer)
+    {
+        $formId = 'contactForm';
+        $captchaModel = Mage::helper('captcha')->getCaptcha($formId);
+        if ($captchaModel->isRequired()) {
+            $controller = $observer->getControllerAction();
+            if (!$captchaModel->isCorrect($this->_getCaptchaString($controller->getRequest(), $formId))) {
+                Mage::getSingleton('customer/session')->addError(Mage::helper('captcha')->__('Incorrect CAPTCHA.'));
+                $controller->setFlag('', Mage_Core_Controller_Varien_Action::FLAG_NO_DISPATCH, true);
+                $controller->getResponse()->setRedirect(Mage::getUrl('*/*/'));
+            }
+        }
+        return $this;
+    }
+}
